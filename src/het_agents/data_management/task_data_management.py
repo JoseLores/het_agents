e09786e72a162e65da13d2c22fba9bc1a219ed27
JoseLores <ldpepe99@gmail.com>
2023-03-29 14:13:25 +0200

    (#4) create functions to read and save data.

--- src/het_agents/data_management/task_data_management.py
+++ src/het_agents/data_management/task_data_management.py
@@ -1,12 +1,11 @@
-"""Tasks for managing the data."""
+"""Tasks for creating the grids that are necessary to solve the models."""
 
-import pandas as pd
 import pytask
 import pickle
 
 from heterogeneous_agents.config import BLD, MODELS, SRC
 from heterogeneous_agents.data_management import produce_grids
-from heterogeneous_agents.utilities import to_pkl
+from heterogeneous_agents.utilities import read_pkl, to_pkl
 
 
 for model in MODELS:
@@ -21,11 +20,9 @@ for model in MODELS:
     @pytask.mark.produces(BLD / "python" / "data" / f"econ_data_{model}_mkt.pkl")
     def task_produce_grids_python(depends_on, produces):
         """Produce all grid, meshes and parameters needed for the model (Python version)."""
-        with open("data", "rb") as f:
-            # Load the dictionaries from the file using the pickle.load() method
-            numerical_params = pickle.load(f)
-            economic_params = pickle.load(f)
+        numerical_params, economic_params = read_pkl("data")
 
         updated_economic_params = produce_grids(economic_params, numerical_params)
 
-        to_pkl(updated_economic_params, produces)
+        to_pkl(numerical_params, updated_economic_params, produces)
+
